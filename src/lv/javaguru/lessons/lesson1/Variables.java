208d192d7018f295dd064fa55e442f8f5474a558
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-08-22 18:26:24 +0300

    #lesson1

---
+++ src/lv/javaguru/lessons/lesson1/Variables.java
@@ -0,0 +1,35 @@
+package lv.javaguru.lessons.lesson1;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        byte numberOfStudents = 10;
+        System.out.println(numberOfStudents);
+        numberOfStudents = 30;
+        System.out.println(numberOfStudents);
+
+        short numberOfHands = 100;
+        System.out.println(numberOfHands);
+
+        int integerNumber = 400;
+        System.out.println(integerNumber);
+
+        long longNumber = 321312312;
+        System.out.println(longNumber);
+
+        boolean hungry = true;
+        System.out.println(hungry);
+
+        char symbolA = 'A';
+        System.out.println(symbolA);
+
+//        String helloWorld = "Hello World";
+//        System.out.println(helloWorld);
+
+        float juniorDeveloperSalary = 700.50f;
+        System.out.println(juniorDeveloperSalary);
+
+        double bankBalance = 10000.73;
+        System.out.println(bankBalance);
+    }
+}
