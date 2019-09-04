79af00b2c6ad313d9ccf06d9a204fe0ced1cb063
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-09-04 20:11:44 +0300

    #lesson3 example for loop

---
+++ src/lv/javaguru/lessons/lesson3/ExampleForLoop.java
@@ -0,0 +1,10 @@
+package lv.javaguru.lessons.lesson3;
+
+class ExampleForLoop {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            System.out.print("i = " + i + ", ");
+        }
+    }
+}
