ceedb41cc5edece2ae8574ae337a8704a1f12865
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-09-04 20:15:28 +0300

    #lesson3 example do-while loop

---
+++ src/lv/javaguru/lessons/lesson3/ExampleDoWhileLoop.java
@@ -0,0 +1,12 @@
+package lv.javaguru.lessons.lesson3;
+
+class ExampleDoWhileLoop {
+
+    public static void main(String[] args) {
+        int counter = 100;
+        do {
+            System.out.println("Hello World!");
+        } while (counter < 100);
+    }
+
+}
