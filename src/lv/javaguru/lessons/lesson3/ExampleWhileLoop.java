6d7077e9e30fada6f937a121ea92694015bfa21d
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2019-09-04 20:06:04 +0300

    #lesson3 example while loop

---
+++ src/lv/javaguru/lessons/lesson3/ExampleWhileLoop.java
@@ -0,0 +1,12 @@
+package lv.javaguru.lessons.lesson3;
+
+class ExampleWhileLoop {
+
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter <= 100) {
+            System.out.println("Counter = " + counter);
+            counter++;
+        }
+    }
+}
